90d6aa6fce9fe711fdd6f4d7c85b7a462f7fa07f
Maga Aubin <magaaubin3@gmail.com>
2026-01-20 23:05:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3699968979-f70a4",
+  "appId": "1:587823340847:web:d69108b19a776ab15ad5da",
+  "apiKey": "AIzaSyBMe7fx_ymOalQAT67ZY0V_i1_toTsTASA",
+  "authDomain": "studio-3699968979-f70a4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "587823340847"
+};
