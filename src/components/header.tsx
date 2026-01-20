8a0cf1c091160e62bd479c3ec79792ecc04f32a2
Maga Aubin <magaaubin3@gmail.com>
2026-01-20 22:29:11 +0000

    what that search bar for ?

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import {
   Bell,
-  Search,
 } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
@@ -15,7 +14,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Input } from '@/components/ui/input';
 import { SidebarTrigger } from './ui/sidebar';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
@@ -32,18 +30,7 @@ export function Header() {
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6">
       <SidebarTrigger />
-      <div className="w-full flex-1">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search bookings..."
-              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-            />
-          </div>
-        </form>
-      </div>
+      <div className="w-full flex-1" />
       <Button variant="ghost" size="icon" className="h-8 w-8">
         <Bell className="h-4 w-4" />
         <span className="sr-only">Toggle notifications</span>
