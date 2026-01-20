e4d2fb4c0b5df732c93b3692567355e7e256d045
Maga Aubin <magaaubin3@gmail.com>
2026-01-20 20:57:07 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import * as React from 'react';
 import {
   Bell,
   Search,
@@ -16,9 +19,16 @@ import { Input } from '@/components/ui/input';
 import { SidebarTrigger } from './ui/sidebar';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
+import { Skeleton } from './ui/skeleton';
 
 export function Header() {
+  const [isMounted, setIsMounted] = React.useState(false);
   const userAvatar = PlaceHolderImages.find(p => p.id === 'user-avatar-1');
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6">
       <SidebarTrigger className="md:hidden" />
@@ -38,24 +48,29 @@ export function Header() {
         <Bell className="h-4 w-4" />
         <span className="sr-only">Toggle notifications</span>
       </Button>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon" className="rounded-full">
-            <Avatar className="h-8 w-8">
-               {userAvatar && <AvatarImage src={userAvatar.imageUrl} alt="User avatar" />}
-              <AvatarFallback>AD</AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Admin Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      
+      {isMounted ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Avatar className="h-8 w-8">
+                {userAvatar && <AvatarImage src={userAvatar.imageUrl} alt="User avatar" />}
+                <AvatarFallback>AD</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Admin Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <Skeleton className="h-8 w-8 rounded-full" />
+      )}
     </header>
   );
 }
