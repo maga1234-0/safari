c4f94fedb5e75a83f7aac444673ebffa5d913f30
Maga Aubin <magaaubin3@gmail.com>
2026-01-22 19:45:59 +0000

    set the toast message in grey

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-muted text-muted-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
