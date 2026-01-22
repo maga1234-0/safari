2db85438b60ac5a718690552262ae555d8142a2f
Maga Aubin <magaaubin3@gmail.com>
2026-01-22 22:39:23 +0000

    when adding this app to home screen put the app icon not the firebase ic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#2B6846" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="PMS safari" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
